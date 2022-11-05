8208a3321d5c70df91a045232b5e4578ae5e0a4b
Alexey Belkov <lexabelco2013@yandex.ru>
2022-11-05 16:31:50 +0500

    feat: добавить каждой карточке свою скорость скролла

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,26 @@
 const cards = document.querySelectorAll('.card');
 
+
+function changedSpeedCardScroll (img, cardImg, speed) {
+  img.src = cardImg.src;
+
+  img.onload = function() {
+    const hight = this.height;
+    cardImg.style = `transition: object-position ${hight * speed}s,
+     -o-object-position ${hight * speed}s`;
+  }
+}
+
 cards.forEach((card) => {
+  const img = new Image();
+
   const cardToggle = card.querySelector('.card__toggle');
-  let cardGithubBlock = card.querySelector('.card__github');
+
   const cardGithubIcon = card.querySelector('.card__github-icon');
   const cardGithubText = card.querySelector('.card__github-text');
+  const cardImg = card.querySelector('.card__img');
+
+  changedSpeedCardScroll(img, cardImg, 0.0005);
 
   cardGithubIcon.addEventListener('mouseover', () => {
     cardGithubText.classList.add('card__github-text_hover');
@@ -16,18 +32,17 @@ cards.forEach((card) => {
 
   if(cardToggle) {
     cardToggle.addEventListener('click', () => {
-      const cardImg = card.querySelector('.card__img');
 
-      if(card.querySelector('.card__toggle_active')) {
-        cardImg.classList.remove('card__img_mobile');
+      if(cardToggle.classList.contains('card__toggle_active')) {
         const cardImgLink = cardImg.src.slice(0, cardImg.src.length - 11);
         cardImg.src = `${cardImgLink}.png`;
+        changedSpeedCardScroll(img, cardImg, 0.0005);
         cardToggle.classList.remove('card__toggle_active');
       }
       else {
-        cardImg.classList.add('card__img_mobile');
         const cardImgLink = cardImg.src.slice(0, cardImg.src.length - 4);
         cardImg.src = `${cardImgLink}-mobile.png`;
+        changedSpeedCardScroll(img, cardImg, 0.001);
         cardToggle.classList.add('card__toggle_active');
       }
 
